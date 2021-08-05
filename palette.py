681e6fc7e000d6dc9bedc5acd559fff891645718
AliA <1638876113@qq.com>
2021-08-05 13:16:02 +0800

    为什么venv传不上去

--- palette.py
+++
@@ -1,7 +0,0 @@
-from app import app, db
-from app.models import Vtuber, Video, Group
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Vtuber': Vtuber, 'Video': Video, 'Group': Group}
